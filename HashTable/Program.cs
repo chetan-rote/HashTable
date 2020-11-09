4fb34717c45514aa6ca521266e3b7f89613bf79c
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-09 15:34:15 +0530

    Add project files.

---
+++ HashTable/Program.cs
@@ -0,0 +1,25 @@
+﻿/******************************************************************************
+ *  Purpose: Implement Hash map using linked list and perform operations on it 
+ *  using key value pair.
+ *
+ *
+ *  @author  Chetan Rote
+ *  @version 1.0
+ *  @since   09-11-2020
+ ******************************************************************************/
+using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome to Hashmap.");
+        }
+    }
+}

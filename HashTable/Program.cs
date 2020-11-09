fad9bcdd6ff01f5b56944e9dd78ec3563fe0ecdd
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-09 16:38:43 +0530

    [Chetan]Add. Get frequency of words.

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -20,6 +20,22 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome to Hashmap.");
+            ///Creating reference of MyMapNode.
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            hash.GetFrequency("To");
+
+            string wordOne = hash.Get("5");
+            Console.WriteLine("5th index value is: " + wordOne);
+            string wordTwo = hash.Get("2");
+            Console.WriteLine("2nd index value is: " + wordTwo);
+
         }
     }
 }
